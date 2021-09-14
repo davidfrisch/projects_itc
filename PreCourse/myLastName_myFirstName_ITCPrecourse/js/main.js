03423f9be94fe1af36dd342da92ebed5c263a2d8
FrischeD <david.frischer@unifr.ch>
2021-09-15 00:11:15 +0200

    finish point Index 1 2 and 3

--- PreCourse/myLastName_myFirstName_ITCPrecourse/js/main.js
+++ PreCourse/myLastName_myFirstName_ITCPrecourse/js/main.js
@@ -1,5 +1,5 @@
 //replace with your user:
-const GITHUB_URL = "https://api.github.com/users/IsraelTechChallengeITC";
+const GITHUB_URL = "https://github.com/davidfrisch";
 
 fetch(GITHUB_URL)
   .then(function(response) {
